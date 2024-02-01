5db7b9ad24c172153300ad486ea4a73f02305086
Fabiano Bispo Canedo <fabiano.canedo@energisa.com.br>
2024-02-01 15:02:08 -0300

    teste

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,13 @@ import { appGymRoutes } from './http/controllers/app_gym/routes';
 import { appFaRoutes } from './http/controllers/app_fa/routes';
 
 
+
+import fastifyStatic from '@fastify/static';
+import fastifyView from '@fastify/view';
+import ejs from 'ejs';
+import path from 'node:path';
+
+
 export const app = fastify();
 
 app.register(fastifyJwt, {
@@ -40,6 +47,27 @@ app.register(fastifyJwt, {
 });
 
 
+// Configuração para servir arquivos estáticos
+app.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/', // Prefixo opcional para servir arquivos estáticos
+  });
+  
+  // Configuração do diretório de views e mecanismo de visualização
+  app.register(fastifyView, {
+    engine: {
+      ejs,
+    },
+    templates: path.join(__dirname, 'views'),
+  });
+  
+  // Rota para a raiz da aplicação
+  app.get('/', async (request, reply) => {
+    reply.view('index.ejs'); // Removido 'pages/' do caminho do modelo
+  });
+  
+
+  
 app.register(fastifyCookie);
 
 app.register(appGymRoutes);
