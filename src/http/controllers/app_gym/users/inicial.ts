37f8bcd9f8df151bbd1ba489f55f495880334745
Fabiano Bispo Canedo <fabiano.canedo@energisa.com.br>
2024-02-01 15:04:01 -0300

    teste

--- src/http/controllers/app_gym/users/inicial.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-
-export async function inicial(request: FastifyRequest, reply: FastifyReply) {
-
-    return reply.status(201).send({mesage: 'Servidor ok'});
-}
-
