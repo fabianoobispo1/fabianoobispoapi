d8e9af2b61775c937f6cc016835c8d070b460759
fabianoobispo1 <fabianoobispo@icloud.com>
2024-02-02 01:27:53 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from './app';
-import { env } from './env';
 
-app.listen({
-    host:'0.0.0.0',
-    port: env.PORT
-}).then(()=> {
+
+app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
     console.log('Servidor rodadndo.✔');
 });
