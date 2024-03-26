301e765bf70a200a16b209dc6edd8bb1fc393e1d
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-03-26 18:20:37 +0000

    teste

--- src/http/controllers/app_fa/faAtleta/faRegistrar.ts
+++ src/http/controllers/app_fa/faAtleta/faRegistrar.ts
@@ -1,23 +1,27 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import {  z } from 'zod';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
-
+import { hash } from 'bcryptjs';
 import { prisma } from '@/lib/prisma';
 
 export async function FaRegistrarAtleta(request: FastifyRequest, reply: FastifyReply) {
     const Tipo = z.enum(['ATAQUE', 'DEFESA', 'ST']);
 
-    const registerBodySchema = z.object({    
+    const registerBodySchema = z.object({ 
+        nome: z.string(),
+        data_nascimento: z.string().datetime(),
+        email: z.string().email(),
+       
         data_inicio: z.string().datetime(),        
         tipo: Tipo,    
         posicao: z.string(),   
         numero: z.number(),
         altura: z.number(),
         pesso: z.number(),
-        faUsuarioId: z.string(),   
+    
     });
 
-    const { data_inicio, tipo, posicao, numero, altura, pesso, faUsuarioId } = registerBodySchema.parse(request.body);
+    const { nome,data_nascimento, email, data_inicio, tipo, posicao, numero, altura, pesso } = registerBodySchema.parse(request.body);
 
     //verifica se o usuario logado e administrador 
     //const usuarioLogado = request.user.sub;
@@ -31,12 +35,33 @@ export async function FaRegistrarAtleta(request: FastifyRequest, reply: FastifyR
         return reply.status(403).send({mesage: 'usuario nao e um admin'});
     }
  */
+    //verifica se o atleta ja tem cadastro como usuario 
+    let faUsuario = await prisma.faUsuario.findUnique({
+        where:{
+            email
+        }
+    })
 
+    //caso nao seja realiza o cadastro do usuario com senha padrao 12345678
+    if (!faUsuario) {
+        const password_hash = await hash('12345678', 6);
+
+        faUsuario = await prisma.faUsuario.create({
+            data:{
+                nome,
+                email,
+                password_hash,
+                data_nascimento
+            }    
+        });
+    }
+
+    //realiza o cadstro do atleta com o id do usuario informado 
     try {
 
         const resutPrisma = await prisma.faAtleta.create({
             data:{
-                data_inicio, tipo, posicao, numero, altura, pesso, faUsuarioId
+                data_inicio, tipo, posicao, numero, altura, pesso, faUsuarioId: faUsuario.id
             }
         });
         return reply.status(201).send(resutPrisma);
